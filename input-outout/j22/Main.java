7fe1644dc42b33b4bbd13ced385e12eb27e94f9d
JaySuryaa-24 <23becy016@kahedu.edu.in>
2025-12-11 12:01:20 +0530

    Add files via upload

---
+++ input-outout/j22/Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int tamil=sc.nextInt();
+		int english=sc.nextInt();
+		int maths=sc.nextInt();
+		int physics=sc.nextInt();
+		int zoology=sc.nextInt();
+	    System.out.println(tamil+'\n'+english+'\n'+maths+'\n'+physics+'\n'+zoology);
+	}
+}
